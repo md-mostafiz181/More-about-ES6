c05773b9f9e77fe38a71fe726a11501a938e19f8
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-10-03 08:11:03 +0600

    added map in an array method

---
+++ map.js
@@ -0,0 +1,11 @@
+const numbers = [2, 4, 6, 7, 8, 9];
+const result = numbers.map(number => number * 2 ) ;
+console.log(result)
+
+const friends = ["Mostafiz", "Rizu ahmend", "Johurul Islam", "Ashraful"] ;
+const firstLetter = friends.map(frnd => frnd[0]);
+console.log(firstLetter);
+
+const count = ["Bangladesh", "Pakistan", "Afganishthan", "Palestine", "Lebanon" ] ;
+const length = count.map(lnth =>lnth.length);
+console.log(length)
